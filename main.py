a5cf349f94229b7910affc89eca7c04c052f9b8d
delgadjd <juliandelgado0102@gmail.com>
2022-10-06 17:19:18 -0500

    added hamming bound

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # This is a series of functions that were implemented during Fall 2022 in
 # MATH 3320: Error-Correcting Codes and Cryptography at Vanderbilt University
+import math
+
+from sympy import N
 
 def error_pattern(codeword1, codeword2):
     if len(codeword1) != len(codeword2):
@@ -7,7 +10,7 @@ def error_pattern(codeword1, codeword2):
         return
     
     print("Error pattern: ", end="")
-    
+
     for i in range(len(codeword1)):
         print(int(codeword1[i] != codeword2[i]), end="")
 
@@ -17,4 +20,29 @@ def information_rate():
 
 
 
-error_pattern("001", "101")
+def hamming_bound(n , d):
+    t = math.floor((d - 1) / 2)
+    numerator = math.pow(2,n)
+    denominator = 0
+    for i in range(0 , t + 1):
+        denominator = denominator + xChooseY(n, i)
+    bound = round(numerator / denominator)
+    return prevPowerOf2(bound)
+
+def factorial(num):
+    if num == 0 or num == 1 :
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+def xChooseY(x, y):
+    return factorial(x) / (factorial(y) * factorial(x - y))
+
+def prevPowerOf2(n):
+    while (n & n - 1):
+        n = n & n - 1       # unset rightmost bit
+ 
+    # `n` is now a power of two (less than or equal to `n`)
+    return n
+
+print(hamming_bound(8,3))

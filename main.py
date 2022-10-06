e1d1b9b322414d56454ba0f04b5c821a90368481
delgadjd <juliandelgado0102@gmail.com>
2022-10-06 18:17:52 -0500

    added perfect codes and cleaned up codebase

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def prevPowerOf2(n):
     return n
 
 def hamming_bound():
-    n, d = input("Enter n, d: ").split()
+    n, d = input("Enter n d: ").split()
     n , d = [int(n), int(d)]
     t = math.floor((d - 1) / 2)
     numerator = math.pow(2,n)
@@ -45,28 +45,44 @@ def hamming_bound():
     for i in range(0 , t + 1):
         denominator = denominator + xChooseY(n, i)
     bound = round(numerator / denominator)
+
+    if(bound == prevPowerOf2(bound)):
+        print("Perfect code!")
+    else:
+        print("Not a perfect code.")
+    print("Hamming Bound: " + str(prevPowerOf2(bound)))
     return prevPowerOf2(bound)
 
 def lower_bound(): #based on Corollary 3.1.14
-    n, d = input("Enter n, d: ").split()
+    n, d = input("Enter n d: ").split()
     n , d = [int(n), int(d)]
     numerator = math.pow(2, n - 1)
     denominator = 0
     for i in range(0, d - 1):
         denominator = denominator + xChooseY(n - 1, i)
     bound = round(numerator / denominator)
-    return int(math.pow(2, math.ceil(math.log(bound) / math.log(2))))
+    lBound = int(math.pow(2, math.ceil(math.log(bound) / math.log(2))))
+    print("Lower bound: " + str(lBound))
+    return lBound
 
 def gilbert_varshamov():
-    n, d = input("Enter n, d: ").split()
+    n, d = input("Enter n d: ").split()
     n , d = [int(n), int(d)]
     k = 2 * d 
     left = 0
     for i in range(0 , d - 1):
         left = left + xChooseY(n - 1, i)
     right = math.pow(2, n-k)
+
+    if(left < right):
+        print("A (%d, %d, %d) code can exist!" % (n, k, d))
+    else: 
+        print("A (%d, %d, %d) code cannot exist!" % (n, k, d))
     return left < right
 
-print(hamming_bound())
-print(lower_bound())
-#print(gilbert_varshamov())
+
+
+#lower_bound()
+hamming_bound()
+#gilbert_varshamov()
+

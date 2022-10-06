38e3b2da10c729ade403507d19b163ff6cc182e2
delgadjd <juliandelgado0102@gmail.com>
2022-10-06 17:41:08 -0500

    added corollary for lower bound

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This is a series of functions that were implemented during Fall 2022 in
 # MATH 3320: Error-Correcting Codes and Cryptography at Vanderbilt University
 import math
+from regex import D
 
 from sympy import N
+
 
 def error_pattern(codeword1, codeword2):
     if len(codeword1) != len(codeword2):
@@ -18,17 +20,6 @@ def error_pattern(codeword1, codeword2):
 def information_rate():
     pass
 
-
-
-def hamming_bound(n , d):
-    t = math.floor((d - 1) / 2)
-    numerator = math.pow(2,n)
-    denominator = 0
-    for i in range(0 , t + 1):
-        denominator = denominator + xChooseY(n, i)
-    bound = round(numerator / denominator)
-    return prevPowerOf2(bound)
-
 def factorial(num):
     if num == 0 or num == 1 :
         return 1
@@ -41,8 +32,30 @@ def xChooseY(x, y):
 def prevPowerOf2(n):
     while (n & n - 1):
         n = n & n - 1       # unset rightmost bit
- 
+
     # `n` is now a power of two (less than or equal to `n`)
     return n
 
-print(hamming_bound(8,3))
+def hamming_bound():
+    n, d = input("Enter n, d: ").split()
+    n , d = [int(n), int(d)]
+    t = math.floor((d - 1) / 2)
+    numerator = math.pow(2,n)
+    denominator = 0
+    for i in range(0 , t + 1):
+        denominator = denominator + xChooseY(n, i)
+    bound = round(numerator / denominator)
+    return prevPowerOf2(bound)
+
+def lower_bound(): #based on Corollary 3.1.14
+    n, d = input("Enter n, d: ").split()
+    n , d = [int(n), int(d)]
+    numerator = math.pow(2, n - 1)
+    denominator = 0
+    for i in range(0, d - 1):
+        denominator = denominator + xChooseY(n - 1, i)
+    bound = round(numerator / denominator)
+    return int(math.pow(2, math.ceil(math.log(bound) / math.log(2))))
+
+print(hamming_bound())
+print(lower_bound())
